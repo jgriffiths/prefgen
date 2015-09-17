0d4dc7d433d7b8d7e3a710dd17d9b52b0ae5abf7
Jon Griffiths <jon_p_griffiths@yahoo.com>
2015-09-17 03:44:51 +0000

    Remove unused import, fix licence version

--- prefgen.py
+++ prefgen.py
@@ -5,7 +5,7 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2.1 of the License, or
+# the Free Software Foundation, either version 2.0 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -20,7 +20,6 @@ import os
 import sys
 import string
 from argparse import ArgumentParser, FileType
-from collections import namedtuple
 
 JAVA_KEYWORDS = [ 'abstract', 'assert', 'boolean', 'break', 'byte',
     'case', 'catch', 'char', 'class', 'const', 'continue', 'default', 'do',
